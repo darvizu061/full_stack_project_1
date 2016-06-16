73cd73507548f89d7f6c8a5fe9655064ae857b41
Danny Arvizu <darvizu061@gmail.com>
2016-06-15 19:14:03 -0500

    Uploading project origin

---
+++ p1/entertainment_center.py
@@ -0,0 +1,17 @@
+import fresh_tomatoes
+import media 
+
+toy_story = media.Movie("Toy Story","A story of a boy and his toys that come to life.","http://www.impawards.com/1995/posters/toy_story_ver1_xxlg.jpg","https://www.youtube.com/watch?v=ZZv1vki4ou4")
+
+avatar = media.Movie("Avatar","A marine on an alien planet.","https://www.movieposter.com/posters/archive/main/98/MPW-49246","https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+
+the_devil_wears_prada = media.Movie("The Devil Wears Prada","A story of a journalist entering the fashion industry.","https://upload.wikimedia.org/wikipedia/en/e/e7/The_Devil_Wears_Prada_main_onesheet.jpg","https://www.youtube.com/watch?v=XTDSwAxlNhc")
+
+
+
+
+
+
+
+movies = [toy_story, avatar, the_devil_wears_prada]
